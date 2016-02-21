123fe2690340d7a88124cf269e356ed1ee90b3bb
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-02-21 07:56:25 +0100

    2016-02-21 some mongoose

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require("express");
+var mongoose = require("mongoose");
+
+var db = mongoose.connect("mongodb://localhost/bookAPI");
+
+// uzywamy modeli, dzięki którym mongoose będzie wiedział jak przetłumaczyć dane, które otrzyma z bazy
+var Book = require("./models/bookModel");
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+// 2:40 + baza
+
+bookRouter.route("/Books")
+    .get(function (req, res) {
+        var responseJson = { firstname: "Harper", lastname: "Lee" };
+        res.json(responseJson);
+    });
+
+app.use("/api", bookRouter);
 
 app.get("/", function (req, res) {
     res.send("welcome to bookapi");
